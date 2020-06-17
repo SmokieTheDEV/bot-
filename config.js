bdfc690b798b1c42e917c2ff0aee0be0c04cb335
Glitch (intermediate-sumptuous-alvarezsaurus) <none>
2020-06-17 16:30:28 +0000

    Import of DevDuvk/QuackBot

---
+++ config.js
@@ -0,0 +1,17 @@
+exports.TOKEN = 'NzE4OTExOTk3NDc0NzY2OTIw.Xub47g.2TQgMh5msLlPxtglRvwHnpELXk8';
+
+exports.PREFIX = 'qq';
+
+exports.GOOGLE_API_KEY = 'AIzaSyDPOmQ5i0FDpH50O4YwtEcSfYw80Izylus';
+
+exports.GENIUS_API_KEY = 'zdTLphFCy4pAlmK1mWEIn5Sw7B1N38XUp8UDeJcUGMavINCWSF3W9VakeQI8Z202';
+
+exports.yandex_API = '7dbe6eb877b74dc4b6e34fe6ec6da86e';
+
+exports.news_API = '7dbe6eb877b74dc4b6e34fe6ec6da86e';
+
+exports.giphy_API = 'CaJxgWS4z2CImmnPsbfLBuGTWLkmElVx';
+
+exports.AME_API = '8428879cb2b45d90d501ca689d3a6a6d46261721e6d0127fa8e83ae3954fb531d29b9ae09886e30833364330494e6234174b973feab81e8b269c0377c670657c';
+
+exports.blague_API = 'rwfcDdWYMrMfdy9WvihcQeEO1-R_s1eh7hNttZFIzPEq5EZDcH2g3WiaO0ccyVml';

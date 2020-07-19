632ea0f57c92abddb156f4225f6afce8b84eb4dc
Glitch (quackbot-v2) <none>
2020-07-19 12:52:34 +0000

    💈🐟 Checkpoint ./events/client/ready.js:18100035/57

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ const { PREFIX } = require('../../config');
 module.exports = async bot => {
     console.log(`${bot.user.username} is available now!`)
     let totalUsers = bot.guilds.cache.reduce((acc, value) => acc + value.memberCount, 0)
-    var activities = [ `${bot.guilds.cache.size} servers`, `${totalUsers} users!`,`ALL THE SERVERS` ], i = 0;
+    var activities = [ `${bot.guilds.cache.size} servers`, `${totalUsers} users!`,`Tapa Tap Tapa Tap Chapa chap` ], i = 0;
     setInterval(() => bot.user.setActivity(`${PREFIX}help | ${activities[i++ % activities.length]}`, { type: "STREAMING" }),5000)
   
 };

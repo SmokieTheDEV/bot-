bbe0b347d146886f1a1fa120a81e12efe0bf70cd
Glitch (quackbot-v2) <none>
2020-08-15 04:40:13 +0000

    🎽🐉 Checkpoint ./config.js:18100035/553 ./Readme.md:18100035/289

---
+++ Readme.md
@@ -0,0 +1,5 @@
+bot is developed by Smokie , 
+you can use this repository , make sure to give me a star
+go to config.js and edit all the secret
+then simply run node . or node.index.js on your editor
+this file is consists of 120 commands , working.
--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-exports.TOKEN = 'NzIzNDgwMjY5MDU0ODA0MDI5.XxQ6tw.SY7e_30mKFgR8CB51tT5e5yCzeQ';
+exports.TOKEN = 'bot token here';
 
-exports.PREFIX = '.';
+exports.PREFIX = 'prefix';
 
-exports.GOOGLE_API_KEY = 'AIzaSyDPOmQ5i0FDpH50O4YwtEcSfYw80Izylus';
+exports.GOOGLE_API_KEY = 'api key';
 
-exports.GENIUS_API_KEY = 'zdTLphFCy4pAlmK1mWEIn5Sw7B1N38XUp8UDeJcUGMavINCWSF3W9VakeQI8Z202';
+exports.GENIUS_API_KEY = 'api key';
 
-exports.yandex_API = '7dbe6eb877b74dc4b6e34fe6ec6da86e';
+exports.yandex_API = 'api key';
 
-exports.news_API = '7dbe6eb877b74dc4b6e34fe6ec6da86e';
+exports.news_API = 'API key';
 
-exports.giphy_API = 'CaJxgWS4z2CImmnPsbfLBuGTWLkmElVx';
+exports.giphy_API = 'API KEY HERE';
 
-exports.AME_API = '8428879cb2b45d90d501ca689d3a6a6d46261721e6d0127fa8e83ae3954fb531d29b9ae09886e30833364330494e6234174b973feab81e8b269c0377c670657c';
+exports.AME_API = 'api key';
 
-exports.blague_API = 'rwfcDdWYMrMfdy9WvihcQeEO1-R_s1eh7hNttZFIzPEq5EZDcH2g3WiaO0ccyVml';
+exports.blague_API = 'api key';
